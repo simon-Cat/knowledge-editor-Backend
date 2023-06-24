28eae088eca0cf16a3eea0f6a407255a846da95b
Bekir Murtazaev <BekirMurtazaev@yandex.ru>
2023-06-24 22:25:58 +0300

    feat: add removing all base of knowledge

---
+++ index.js
@@ -0,0 +1,74 @@
+// подключили express
+const express = require('express');
+// подключили модуль fs
+const fs = require('fs');
+
+// здесь храним всю базу знаний
+let baseOfKnowledge;
+
+// чтение файла со знаниями
+// сохранение в переменную baseOfKnowledge
+fs.readFile('./baseOfKnowledge.json', (err, data) => {
+  // если ошибка чтения
+  // выбрасываем ошибку
+  if (err) throw err;
+
+  // сохранение базы знаний
+  baseOfKnowledge = JSON.parse(data);
+});
+
+const PORT = 3000;
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// обработка get-запроса по url - "/"
+// отправляем всю базу знаний на клиент
+app.get('/', (req, res) => {
+  res.send(baseOfKnowledge);
+});
+
+// обработка post-запроса по url - "/"
+app.post('/', function (req, res) {
+  // переменная для хранения данных
+  // от клиента
+  let chunked = '';
+  // ждем, когда данные придут
+  req.on('data', (chunk) => {
+    chunked += chunk;
+  });
+  // данные пришли
+  req.on('end', () => {
+    // получаем id конкретноы
+    // базы знаний
+    const { id } = JSON.parse(chunked);
+
+    // индеск элемента в базе знаний
+    const index = baseOfKnowledge.findIndex((element) => element.id === id);
+
+    // удаляем базу знаний по индексу index
+    baseOfKnowledge.splice(index, 1);
+
+    // создаем копию всей базы знаний
+    // переводим в фромат JSON
+    const copyOfBaseOfKnowedge = JSON.stringify(baseOfKnowledge);
+
+    // записываем новые данные в файл с базой знаний
+    fs.writeFile('./baseOfKnowledge.json', copyOfBaseOfKnowedge, (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+
+    // отправляем клиенту
+    // обновленные данные базы знаний
+    res.send(baseOfKnowledge);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('listen port ' + PORT);
+});
